f0b23acdf8ea7c1300c13aab17d873aa87f8ebb2
Dynamic Host Configured Pizza <59972478+DHCPizza@users.noreply.github.com>
2023-09-22 11:24:54 -0400

    v1 Right Click Teams Dial

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    "id": "dialWithTeams",
+    "title": "Dial with Teams",
+    "contexts": ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === "dialWithTeams" && info.selectionText) {
+    // Remove any white spaces or encoded spaces (%20)
+    const cleanedNumber = info.selectionText.replace(/%20|\s/g, "");
+    const telLink = "tel:" + cleanedNumber;
+    chrome.tabs.create({ url: telLink });
+  }
+});
